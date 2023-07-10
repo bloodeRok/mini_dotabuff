9e2211a0bf2d3c0ac006d28e354c6cf150d832e4
Sergey Poluyakhtov <sergey.poluyakhtov@lanit-tercom.com>
2023-07-10 03:46:48 +0300

    Initial commit.
    (cherry picked from commit 0465c60b1a2e1ac3ff7466c7712eb3d2c1749f1c)

--- server/settings.py
+++ server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-z#kcf3a4)kaivc*^-rzpfm5zqgkw4kf&t9xgh-z%7x21pj&gs!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
