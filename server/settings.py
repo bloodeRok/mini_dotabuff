d671658a79efde53db0a588841f3c7e3d564fa59
bloodeRok <spoluyakhtov99@gmail.com>
2023-10-13 16:47:18 +0300

    Temp.

--- server/settings.py
+++ server/settings.py
@@ -120,8 +120,8 @@ DATABASES = {
         "NAME": os.environ.get("POSTGRES_DB", ""),
         "USER": os.environ.get("POSTGRES_USER", ""),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD", ""),
-        "HOST": os.environ.get("POSTGRES_HOST", ""),
-        "PORT": os.environ.get("POSTGRES_PORT", ""),
+        "HOST": os.environ.get("DATABASE_HOST", ""),
+        "PORT": os.environ.get("DATABASE_PORT", ""),
     }
 }
 
